698d879dc2e76e8418d7660e33da2545ab055340
Mike Splain <msplain@paypal.com>
2013-11-20 14:03:25 -0500

    Fix tests

--- test/spec/default_spec.rb
+++ test/spec/default_spec.rb
@@ -8,6 +8,13 @@ describe 'property_file_test::default' do
       ChefSpec::Runner.new(step_into: ['property_file']).converge(described_recipe)
     end
 
+    before do
+      stub_data_bag_item("property_files", "my_application_group").and_return({"id" => "my_application_group","properties" => {"a" => "aaaa","b" => "bbbb","aa" => "sdfdf","Apps" => {"Name" => "a","property" => "bbc"}}})
+      stub_data_bag_item("property_files", "my_application_group_my_awesome_application").and_return({"id" => "my_application_group_my_awesome_application","properties" => {"a" => "bbb","b" => "ccc","e" => "zzz"}})
+      stub_data_bag_item("property_files", "my_application_group_my_awesome_application_instance01").and_return({"id" => "my_application_group_my_awesome_application_instance01","properties" => {"a" => "aaaa","b" => "bbbb"}})
+      stub_command("test -e /home/test_user").and_return(false)
+    end
+
     it 'creates the app user' do
       expect(chef_run).to create_user('test_user')
     end
